4f55610835cd6a3063bd0a92c0eff4fc8cbeaad1
Mateusz Motkowicz <mma.style113@gmail.com>
2021-06-14 14:50:50 +0200

    Wrappers

---
+++ Wrappers/GetNodeTreeWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeStructure.Models;
+
+namespace TreeStructure.Wrappers
+{
+    public class GetNodeTreeWrapper
+    {
+        public Guid Guid { get; set; }
+        public GetNodeTreeDto NodeTree { get; set; }
+    }
+}
