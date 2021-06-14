26b253025c936eff59e9e2beb9de05288d2fb6ae
Mateusz Motkowicz <mma.style113@gmail.com>
2021-06-14 18:13:18 +0200

    Account

---
+++ Serveces/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeStructure.Models;
+
+namespace TreeStructure.Serveces
+{
+    public interface IAccountService
+    {
+        public void RegisterUser(RegisterUserDto dto);
+        //public string GenerateJwt(LoginDto dto);
+    }
+}
