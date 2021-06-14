7c64516d219ca12b4d01e0a0221a16f38e682035
Mateusz Motkowicz <mma.style113@gmail.com>
2021-06-14 14:47:26 +0200

    Entities

---
+++ Entities/NodeTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeStructure.Models;
+
+namespace TreeStructure.Entities
+{
+    [Table("NodeTrees")]
+    public class NodeTree
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Guid { get; set; }
+
+        public string stringObjectNodeTree { get; set; }
+    }
+}
---
+++ Entities/TreeStructureDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeStructure.Entities
+{
+    public class TreeStructureDbContext : DbContext
+    {
+        public TreeStructureDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<NodeTree> NodeTrees { get; set; }
+    }
+}
