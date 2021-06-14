004f57bab35db1607d4ea446ba6e9d3c6ddf1508
Mateusz Motkowicz <mma.style113@gmail.com>
2021-06-14 21:07:52 +0200

    Add props

--- Models/RegisterUserDto.cs
+++ Models/RegisterUserDto.cs
@@ -12,7 +12,9 @@ namespace TreeStructure.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public DateTime? DateOfBirth { get; set; }
-        public int RoleId { get; set; } = 1;
+        public int RoleId { get; set; }
     }
 }
