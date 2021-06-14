c3ff1cfcebed56dedae818ce35877f8f433cbea0
Mateusz Motkowicz <mma.style113@gmail.com>
2021-06-14 14:47:41 +0200

    Installers

---
+++ Installers/MvcInstaller.cs
@@ -0,0 +1,31 @@
+﻿using TreeStructure.Entities;
+using TreeStructure.Serveces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TreeStructure.Repositories;
+
+namespace TreeStructure.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TreeStructureDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("TreeStructureCsWin")));
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddScoped<INodeTreeService, NodeTreeService>();
+            services.AddScoped<INodeTreeRepository, NodeTreeRepository>();
+
+            services.AddControllers();
+        }
+    }
+}
---
+++ Installers/SwaggerInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeStructure.Installers
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tree Structure API", Version = "v1" });
+            });
+        }
+    }
+}
